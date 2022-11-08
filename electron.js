47da783182ce9b06da661117dc4106560a94faaf
Edward Glendinning <edward82009@hotmail.com>
2022-11-08 15:03:31 +0000

    badly done electron implementation

---
+++ electron.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.webContents.openDevTools();
+    win.loadFile('./public/index.html');
+}
+
+app.on('ready', () => {
+    createWindow();
+})
+
+app.on('window-all-closed', app.quit);
--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const webpack = require("webpack");
 module.exports = {
     entry: "./index.js",
     mode: "development",
+    target: "electron",
     module: {
         rules: [
             {
